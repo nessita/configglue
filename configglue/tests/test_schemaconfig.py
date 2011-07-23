95c2c38badfc1fd43922614adc3743b37684581d
Ricardo Kirkner <ricardo.kirkner@canonical.com>
2011-07-23 18:42:06 +0000

    remove unnecessary code
    the setUp/tearDown methods may be an artifact from an unclean merge.
    removing as they don't contribute to the code and are erroring out.

--- configglue/tests/test_schemaconfig.py
+++ configglue/tests/test_schemaconfig.py
@@ -336,13 +336,6 @@ class TestSchemaConfigGlue(unittest.TestCase):
 
 
 class ConfigglueTestCase(unittest.TestCase):
-    def setUp(self):
-        self.mock_argv = patch_object(sys, 'argv', ['foo'])
-        self.mock_argv.start()
-
-    def tearDown(self):
-        self.mock_argv.stop()
-
     @patch('configglue.glue.SchemaConfigParser')
     @patch('configglue.glue.schemaconfigglue')
     def test_configglue_no_errors(self, mock_schemaconfigglue,
