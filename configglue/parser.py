bae4fe38adb997ed3ff07387fc5b05c24d0ae23e
Ricardo Kirkner <ricardo@kirkner.com.ar>
2012-06-15 11:04:51 -0300

    use a custom logging to allow more fine-grained control

--- configglue/parser.py
+++ configglue/parser.py
@@ -30,8 +30,6 @@ from ConfigParser import (
     NoSectionError,
 )
 
-from configglue.schema import DictOption
-
 
 __all__ = [
     'SchemaValidationError',
@@ -39,6 +37,8 @@ __all__ = [
 ]
 
 CONFIG_FILE_ENCODING = 'utf-8'
+
+logger = logging.getLogger(__name__)
 
 
 class SchemaValidationError(Exception):
@@ -247,7 +247,7 @@ class SchemaConfigParser(BaseConfigParser, object):
             try:
                 fp = codecs.open(path, 'r', CONFIG_FILE_ENCODING)
             except IOError:
-                logging.warn(
+                logger.warn(
                     'File {0} could not be read. Skipping.'.format(path))
                 continue
             self._read(fp, path, already_read=already_read)
