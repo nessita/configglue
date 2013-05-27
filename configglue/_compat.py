d23fa729c74367771d6388ca77a5ab2c0d7b933e
Ricardo Kirkner <ricardo.kirkner@canonical.com>
2013-05-26 21:56:38 -0300

    removed unnecessary compat code

--- configglue/_compat.py
+++ configglue/_compat.py
@@ -7,13 +7,9 @@ if not PY2:
     text_type = str
     string_types = (str,)
     import builtins
-    iterkeys = lambda d: iter(d.keys())
-    itervalues = lambda d: iter(d.values())
     iteritems = lambda d: iter(d.items())
 else:
     text_type = unicode
     string_types = (str, unicode)
     import __builtin__ as builtins
-    iterkeys = lambda d: d.iterkeys()
-    itervalues = lambda d: d.itervalues()
     iteritems = lambda d: d.iteritems()
