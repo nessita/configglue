564c27a4bd3c02c0c41088263811d3c2d1cc97c3
Natalia B. Bidart <natalia.bidart@canonical.com>
2013-05-30 16:41:51 -0300

    Added missing openid_email_whitelist_regexp_list option to the openid schema.

--- configglue/tests/test_contrib_schema.py
+++ configglue/tests/test_contrib_schema.py
@@ -9,5 +9,11 @@ class DjangoOpenIdAuthSchemaTestCase(TestCase):
     def test_openid_launchpad_teams_required_option(self):
         schema = DjangoOpenIdAuthSchema()
         option = schema.openid.openid_launchpad_teams_required
-        self.assertTrue(isinstance(option, ListOption))
-        self.assertTrue(isinstance(option.item, StringOption))
+        self.assertIsInstance(option, ListOption)
+        self.assertIsInstance(option.item, StringOption)
+
+    def test_openid_email_whitelist_regexp_list_option(self):
+        schema = DjangoOpenIdAuthSchema()
+        option = schema.openid.openid_email_whitelist_regexp_list
+        self.assertIsInstance(option, ListOption)
+        self.assertIsInstance(option.item, StringOption)
